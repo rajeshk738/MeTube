25aa7972d6dc84d1d3d36462f100b72d778d4da4
TarunUpp <tupputu@clemson.edu>
2022-04-16 15:25:20 -0400

    commit userdetails

--- files/Classes/UserDetails.php
+++ files/Classes/UserDetails.php
@@ -5,18 +5,18 @@ class UserDetails{
     private $userData;
     public function __construct($con,$userName){
         $this->con = $con;
-        $query = $this->con->prepare("SELECT * FROM users where userName = '$userName'");
+        $query = $this->con->prepare("SELECT * FROM users where username = '$userName'");
         $query->execute();
         $this->userData = $query->fetch(PDO::FETCH_ASSOC);
     }
     public function getUserName(){
-        return $this->userData["userName"];
+        return $this->userData["username"];
     }
     public function getEmail(){
-        return $this->userData["emailId"];
+        return $this->userData["email"];
     }
     public function getjoinDate(){
-        return $this->userData["joinDate"];
+        return $this->userData["signUpDate"];
     }
 
 }
