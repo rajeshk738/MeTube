a969f4b9f2475104356a42718a8284750014f2e0
rajeshk738 <kandimalla.rajesh2010@gmail.com>
2022-04-19 23:11:52 -0400

    newfiles

---
+++ discussion/discussionBoard.php
@@ -0,0 +1,25 @@
+<?php
+require_once("other/connection.php");
+require_once("other/main.php");
+$name= $loggedInUser->getusername();
+?>
+<div class="container">
+
+<div class="panel panel-default" style="margin-top:50px">
+  <div class="panel-body">
+    <h3>Discussion Board</h3>
+    <hr>
+    <form name="frm" method="post">
+        <input type="hidden" id="commentid" name="Pcommentid" value="0">
+	<div class="form-group">
+	  <label for="usr">Write your name:</label>
+	  <input type='text' class='form-control' name='name' value= "+$name+" required>
+	</div>
+    <div class="form-group">
+      <label for="comment">Write your question:</label>
+      <textarea class="form-control" rows="5" name="msg" required></textarea>
+    </div>
+	 <input type="button" id="butsave" name="save" class="btn btn-primary" value="Send">
+  </form>
+  </div>
+</div>
--- discussionBoard.php
+++ discussionBoard.php
@@ -3,23 +3,36 @@ require_once("other/connection.php");
 require_once("other/main.php");
 $name= $loggedInUser->getusername();
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" href="./images/favicon.png" type="image/png" sizes="16x16">
+<title>forum</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<script src="main.js"></script>
+</head>
+
 <div class="container">
 
 <div class="panel panel-default" style="margin-top:50px">
   <div class="panel-body">
-    <h3>Discussion Board</h3>
-    <hr>
-    <form name="frm" method="post">
-        <input type="hidden" id="commentid" name="Pcommentid" value="0">
-	<div class="form-group">
-	  <label for="usr">Write your name:</label>
-	  <input type='text' class='form-control' name='name' value= "+$name+" required>
-	</div>
-    <div class="form-group">
-      <label for="comment">Write your question:</label>
+
+    <form action='save.php' method="post">
+        <input type="hidden" id="id" name="id" value="0">
+
+	  <label for="name">Name:</label>
+	  <input type='text' class='form-control' name='name'required>
+
+
+      <label for="msg">Question:</label>
       <textarea class="form-control" rows="5" name="msg" required></textarea>
-    </div>
-	 <input type="button" id="butsave" name="save" class="btn btn-primary" value="Send">
+
+	 <input type="button" class="btn btn-primary" value="Submit">
   </form>
-  </div>
+
+</div>
 </div>
